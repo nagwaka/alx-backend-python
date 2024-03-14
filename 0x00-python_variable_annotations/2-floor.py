638722dfa23d6bce7b9877028023da6efdd1bc6a
Aziz <ngwk.zz@outlook.com>
2024-03-14 19:55:06 +0000

    Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -7,7 +7,7 @@ as argument and returns the floor of the float
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """
     takes a float n as argument and returns the floor of the float
     """
